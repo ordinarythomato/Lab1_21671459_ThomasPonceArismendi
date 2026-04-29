c30130e32bdb8a7d9e7bfdc489cb089b5d18cc72
ordinarythomato <ponceathomas@gmail.com>
2026-04-28 20:41:11 -0400

    Avance en TDA CARTA POKEMON (Consolidación de primeras 4 capas de TDA) (28/04/26)

--- TDA CARTA POKEMON-L1.rkt
+++ TDA CARTA POKEMON-L1.rkt
@@ -4,19 +4,76 @@
 (define MIN_PS 0) ; Punto de salud más bajo para una Carta Pokémon
 (define MIN_ATAQUE 0) ;Ataque más bajo que puede influir en Carta Pokémon
 (define TiposElementales '("Agua" "Fuego" "Eléctrico" "Tierra" "Metal" "Normal" "Lucha" "Psíquico" "Planta" )); Tipos Elementales para un Pokémon
-;"Getters" para TDA
+(define Niveles '("Básico" "Fase 1" "Fase 2" "Básico Ex" "Fase 1 Ex" "Fase 2 Ex"))
+
+;; CAPAS DE TDA CARTAS ;;
+
+;; CAPA SELECTORA ;;
+; |OBSERVACIÓN|: Los Selectores (o "Getters") se han definido primero porque ayudan a armar y validar las funciones de las capas sucesoras (Los Constructores, los de Pertenencia y demás)
+
 (define GetNombre (lambda (Carta) (car Carta))) ;Obtener nombre de carta (1er elemento)
+(define EsTipoElementalValido? ;Función que apoya a GetTipoElemental, que permite obtener el Tipo Elemental de una Carta Pokemon
+  (lambda (Carta)
+    (if (member (cadr Carta) TiposElementales); se usa la función member de la forma (member elemento lista), si elemento pertenece a lista se interpreta como un output #t
+        #t
+        #f
+        )))
 (define GetTipoElemental
   (lambda (Carta) ; Obtener Tipo Elemental (2do elemento), consultando: Pertenece el segundo elemento de la carta a la lista de Tipos Elementales
-    (if (member (cadr Carta) TiposElementales); se usa la función member de la forma (member elemento lista), si elemento pertenece a lista se interpreta como un output #t
+    (if (EsTipoElementalValido? Carta); Se usa la función member de la forma (member elemento lista), si elemento pertenece a lista se interpreta como un output #t
         (cadr Carta)
-        null
+        (raise "Elemento incosistente con Cartas Pokemon")
         )
     ))
 (define GetPS (lambda (Carta) (caddr Carta))); Obtener Puntos de Salud (Tercer elemento)
 (define GetAtaque (lambda (Carta) (cadddr Carta))); Obtener Ataque (Cuarto elemento)
-(define GetNivel (lambda (Carta) (car(cddddr Carta)))) ; Obtener Nivel de Carta (Quinto elemento)
+(define EsNivelValido? ;Función que apoya a GetNivel, que permite obtener el Nivel de una Carta Pokemon (Básico, Fase 1 o Fase 2)
+  (lambda (Carta)
+    (if (member (car(cddddr Carta)) Niveles)
+        #t
+        #f)
+    ))
+(define GetNivel ; Obtener Nivel de Carta (Quinto elemento)
+  (lambda (Carta)
+    (if (EsNivelValido? Carta)
+        (car(cddddr Carta))
+        (raise "Nivel incosistente con Cartas Pokémon"))
+    )) 
 (define GetHabilidad (lambda (Carta) (cadr(cddddr Carta)))); Obtener Habilidad de Carta (Sexto elemento)
+
+;; CAPA DE MODELACIÓN ;;
+(define CartaPokemonVacia ;Función modeladora, que muestra la estructura y cantidad de variables de una Carta Pokémon sin considerar valores de entrada
+  (lambda ()
+    (list "Sin Nombre" "Normal" 0 0 "Básico" null)
+    )
+  )
+;; CAPA DE CONSTRUCCIÓN ;;
+(define CartaPokemon; Función constructora de una carta Pokémon, que permite construir la carta con valores de entrada
+  (lambda (Nombre TipoElemental PS Ataque Nivel Habilidad)
+   (list Nombre TipoElemental PS Ataque Nivel Habilidad)
+    ; La Carta Pokémon será una lista con los atributos: Nombre, TipoElemental, PS, Ataque, Habilidad y Nivel.
+   )
+ )
+;; CAPA DE PERTENENCIA ;;
+(define EsCartaPokemon? ; Función verificadora para una carta Pokémon
+ (lambda (CartaPokemon)
+   (if (and (string? (GetNombre CartaPokemon)) ;Es el primer elemento de la Carta un string? (Nombre)
+            (EsTipoElementalValido? CartaPokemon) ;Es el segundo elemento de la Carta un string? (TipoElemental)
+            (and (integer? (GetPS CartaPokemon))
+                 (or (>= (GetPS CartaPokemon) MIN_PS)))
+            ; Es el tercer elemento de la Carta, PS, un entero mayor o igual a MIN_PS? (PS: Puntos de Salud, MIN_PS: Mínimo Punto de Salud)
+            (and (integer?(GetAtaque CartaPokemon))
+                     (or (>= (GetAtaque CartaPokemon) MIN_ATAQUE)))
+            ; Es el 4to elemento de la Carta, Valor de Ataque, un entero mayor o igal a MIN_ATAQUE? (MIN_ATAQUE: Mínimo valor de ataque)
+            (EsNivelValido? CartaPokemon) ; Es el 5to elemento de la Carta un string? (Detalles de Nivel)
+            (or (string? (GetHabilidad CartaPokemon)) (null? (GetHabilidad CartaPokemon))) ; Es el 6to elemento de la Carta un string o vacío? (Detalles de Habilidad, opcional)
+            )
+       #t
+       #f
+       )))
+
+;; CAPA DE OTRAS FUNCIONES ;;
+; Función de Escáner Pokedex
 (define Pokedex ;FUNCIÓN POKEDEX: Imprime por consola los resultados de las funciones Getters aplicadas a una carta Pokémon
   (lambda (CartaPokemon)
     (if (EsCartaPokemon? CartaPokemon)
@@ -24,40 +81,26 @@
               "Tipo Elemental: " (GetTipoElemental CartaPokemon) "|"
               "Puntos de Salud: " (GetPS CartaPokemon) "|"
               "Ataque: " (GetAtaque CartaPokemon) "|"
+              "Nivel: " (GetNivel CartaPokemon) "|"
               "Habilidad: " (GetHabilidad CartaPokemon)
               ))
-        (displayln "Datos Inexistentes de Carta Pokémon")
+        (raise "Datos Inexistentes de Carta Pokémon")
         )
     )
   )
-;; CAPAS DE TDA CARTAS ;;
-;CAPA REPRESENTADORA
-(define CartaPokemon; Función representadora de una carta Pokémon
-  (lambda (Nombre TipoElemental PS Ataque Nivel Habilidad)
-   (list Nombre TipoElemental PS Ataque Nivel Habilidad)
-    ; La Carta Pokémon será una lista con los atributos: Nombre, TipoElemental, PS, Ataque, Habilidad y Nivel.
-   )
- )
-; CAPA IDENTIFICADORA
-(define EsCartaPokemon? ; Función identificadora para una carta Pokémon
- (lambda (CartaPokemon)
-   (if (and (string? (GetNombre CartaPokemon)) ;Es el primer elemento de la Carta un string? (Nombre)
-            (string? (GetTipoElemental CartaPokemon)) ;Es el segundo elemento de la Carta un string? (TipoElemental)
-            (integer? (GetPS CartaPokemon)); Es el tercer elemento de la Carta un entero? (PS: Puntos de Salud)
-            (integer?(GetAtaque CartaPokemon)); Es el 4to elemento de la Carta un entero? (Valor de Ataque)
-            (string? (GetNivel CartaPokemon)) ; Es el 5to elemento de la Carta un string? (Detalles de Nivel)
-            (string? (GetHabilidad CartaPokemon)) ; Es el 6to elemento de la Carta un string? (Detalles de Habilidad)
-            )
-       #t
-       #f
-       )))
-
-
-;; Ejecución
-(define Pikachu(CartaPokemon "Pikachu" "Eléctrico" 150 60 "Basic" "Thunders"))
-(define Charmander (CartaPokemon "Charmander" "Fuego" 80 50 "Basic" "Fire Spitting"))
-(define Torchic (CartaPokemon "Torchic" "Fuego" 60 10 "Basic" "Call the Family"))
+;; Definiendo Cartas
+(define Pikachu(CartaPokemon "Pikachu" "Eléctrico" 150 60 "Básico" "Thunders"))
+(define Charmander (CartaPokemon "Charmander" "Fuego" 80 50 "Básico" "Fire Spitting"))
+(define Torchic (CartaPokemon "Torchic" "Fuego" 60 10 "Básico" "Call the Family"))
+;Ejecutando funciones en cartas definidas
 (Pokedex Pikachu)
 (Pokedex Charmander)
 (Pokedex Torchic)
 (GetTipoElemental Charmander)
+(define Void (CartaPokemonVacia))
+(EsCartaPokemon? Void)
+;; PENDIENTE ;;
+;Agregar un nuevo atributo "booleano" que indique si la carta es Ex o no (hay consideración)
+;Plantear mejor el apartado de Habilidad (pensando que puede ser opcional, también es agregable un apartado booleano)
+;Empezar a trabajar en funciones modificadoras del TDA
+;Empezar a avanzar un TDA juego, TDA jugador, TDA Carta de Energía, TDA Carta de Entrenador
